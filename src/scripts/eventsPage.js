34bf68ab6df943502e1759fc4ca85b14003cb75e
bossSteve03 <stotev0@gmail.com>
2023-03-29 16:56:27 +0100

    yeah

--- src/scripts/eventsPage.js
+++ src/scripts/eventsPage.js
@@ -80,11 +80,11 @@ document.addEventListener('DOMContentLoaded', async () => {
 //Get modal element
 const modal = document.getElementById('eventModal');
 //get open modal button
-const modalBtn = document.getElementById('modalBtn');
+const event = document.getElementById('modalBtn');
 //get close button
 const closeBtn = document.getElementById('closeBtn');
 //Listen for click
-modalBtn.addEventListener('click', openModal);
+allEventsEl.addEventListener('click', openModal);
 //Function to open modal
 function openModal() {
     modal.style.display = 'block';
